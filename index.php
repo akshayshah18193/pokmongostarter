f364c9c8b1b3ff664cc18483744a3c656df78c60
akshayshah18193 <akshayshah.1893@gmail.com>
2016-07-17 03:18:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -31,15 +31,15 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
       <div class="item active">
-        <img src="C:\Users\System_adminAk\Desktop\Akshay\img1.jpg" alt="Pokemon" width="460" height="345">
+        <img src="img1.jpg" alt="Pokemon" width="460" height="345">
       </div>
 
       <div class="item">
-        <img src="C:\Users\System_adminAk\Desktop\Akshay\img1.jpg" alt="Pokemon" width="460" height="345">
+        <img src="img1.jpg" alt="Pokemon" width="460" height="345">
       </div>
     
       <div class="item">
-        <img src="C:\Users\System_adminAk\Desktop\Akshay\img.jpg" alt="Flower" width="460" height="345">
+        <img src="img.jpg" alt="Flower" width="460" height="345">
       </div>
 
       
